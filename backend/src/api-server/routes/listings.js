d7ebed925518758ed298c8ca9a9679a3ab59adab
Severn Lortie <severn.lortie@gmail.com>
2024-12-29 22:40:32 -0500

    feat(api-server): return listings in reverse chronological order by creation date

--- backend/src/api-server/routes/listings.js
+++ backend/src/api-server/routes/listings.js
@@ -4,7 +4,7 @@ export function getRoutes() {
   const router = express.Router();
   router.get('/', async (req, res) => {
     try {
-      const listings = await req.db.query('SELECT * FROM listings;');
+      const listings = await req.db.query('SELECT * FROM listings ORDER BY createdat DESC;');
       res.json(listings.rows);
     } catch (error) {
       res.status(500).send('Server encountered an error trying to get latest listings.');
