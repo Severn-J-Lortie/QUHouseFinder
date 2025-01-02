6f56f8a1f282de61b3f4699f111f78e432389396
Severn Lortie <severn.lortie@gmail.com>
2025-01-01 22:22:33 -0500

    bugfix(web-scraper): update Panadew for their new website

--- backend/src/web-scraper/data-sources/Panadew.js
+++ backend/src/web-scraper/data-sources/Panadew.js
@@ -1,40 +1,22 @@
 import { Datasource } from '../Datasource.js';
 export class Panadew extends Datasource {
-
-  
   constructor() {
     const selectors = {
-      _listingElements: { 
-        selector: 'form div.four.columns.listingblockgrid.listingblock',
-        filter: listing => !listing.querySelector('div:nth-child(1) > a > div > div')
-      },
-      _link: { selector: 'div > a', getProperty: el => el.href },
-      address: '#title',
+      _links: '.featured-listing',
+      address: { selector: '.title', getProperty: el => el.textContent.split('\n')[1] },
       leaseStartDate: {
-        selector: '#tab0 > ul > li:nth-child(5)',
-        getProperty: el => el.textContent.split(':')[1].trim()
+        selector: '.unit-detail__available-date.text--muted',
+        getProperty: el => el.textContent.split(' ')[1].trim()
       },
-      beds: { selector: '#tab0 > ul > li:nth-child(3)', getProperty: el => el.textContent.split(':')[1].trim() },
-      baths: { selector: '#tab0 > ul > li:nth-child(4)', getProperty: el => el.textContent.split(':')[1].trim() },
-      totalPrice: 'span.featuresprice',
-      description: { selector: 'div[itemprop="description"]', getProperty: el => el.textContent.trim().replace('\n', ' ')}
+      beds: '.unit-detail__unit-info > li',
+      baths: '.unit-detail__unit-info > li:nth-child(2)',
+      totalPrice: '.unit-detail__price.accent-color.title--small',
+      description: '.unit-detail__description'
     }
     super(
       'Panadew Property Management', 
-      'https://www.panadew.ca/property_type/student-rentals', 
+      'https://panadew.managebuilding.com/Resident/public/rentals', 
       selectors
     );
-  }
-  async fetchListings() {
-    let pageNumber = 1;
-    let allListings = [];
-    let pageListings = [];
-    while (pageNumber < 2 || pageListings.length) {
-      const link = `${this.link}/page/${pageNumber}`;
-      pageListings = await super.fetchListings(link, pageNumber)
-      allListings = allListings.concat(pageListings)
-      pageNumber++;
-    }
-    return allListings;
   }
 }
